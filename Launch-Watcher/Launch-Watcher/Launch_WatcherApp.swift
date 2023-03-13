305ba733fc721963d50f8b466352db33455f2450
Anderson Maia <andersonmaia@Poas-MacBook-Pro.local>
2023-03-13 16:31:04 -0300

    Initial commit

---
+++ Launch-Watcher/Launch-Watcher/Launch_WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Launch_WatcherApp.swift
+//  Launch-Watcher
+//
+//  Created by Anderson Maia on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Launch_WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
